db617a323d36d13e1eea5e11475a4e2f60d4628a
Dao Heng Liu <daohengliu@gmail.com>
2023-03-03 11:08:32 +0000

    add support for animated flags using sequences of PNGs

--- osu.Game.Tournament/Components/DrawableTeamFlag.cs
+++ osu.Game.Tournament/Components/DrawableTeamFlag.cs
@@ -3,6 +3,9 @@
 
 #nullable disable
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using JetBrains.Annotations;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
@@ -10,7 +13,8 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
-using osu.Game.Tournament.IO;
+using osu.Framework.Logging;
+using osu.Game.Skinning;
 using osu.Game.Tournament.Models;
 using osuTK;
 
@@ -23,15 +27,13 @@ namespace osu.Game.Tournament.Components
         [UsedImplicitly]
         private Bindable<string> flag;
 
-        // private PoCFlagContainer flagContainer;
-
         public DrawableTeamFlag(TournamentTeam team)
         {
             this.team = team;
         }
 
         [BackgroundDependencyLoader]
-        private void load(TextureStore textures, TournamentVideoResourceStore storage)
+        private void load(TextureStore textures)
         {
             if (team == null) return;
 
@@ -40,35 +42,91 @@ namespace osu.Game.Tournament.Components
                 Size = new Vector2(67, 67);
             }
 
-            Masking = true;
+            Masking = false;
             CornerRadius = 5;
 
             (flag = team.FlagName.GetBoundCopy()).BindValueChanged(_ =>
             {
-                if (storage.GetStream(team.FlagName.Value) != null)
-                {
-                    Child = new TourneyVideo(team.FlagName.Value) { Loop = true, RelativeSizeAxes = Axes.Both };
-                }
-                else
-                {
-                    var newTexture = textures.Get($@"Flags/{flag}");
+                InternalChild = GetFlag(textures, Size);
+            }, true);
+        }
 
-                    if (newTexture == null)
-                    {
-                        return;
-                    }
+        // most of the code below is stolen from osu.Game.Skinning.LegacySkinExtensions
+        public Drawable GetFlag(TextureStore textureStore, Vector2 size)
+        {
+            var textures = GetTextures(textureStore);
 
-                    var sprite = new Sprite
+            switch (textures.Length)
+            {
+                case 0:
+                    return Empty();
+
+                case 1:
+                    return new Sprite
                     {
-                        RelativeSizeAxes = Axes.Both,
+                        Texture = textures[0],
+                        Size = size,
                         Anchor = Anchor.Centre,
                         Origin = Anchor.Centre,
                         FillMode = FillMode.Fill
                     };
-                    sprite.Texture = newTexture;
-                    Child = sprite;
+
+                default:
+                    var animation = new LegacySkinExtensions.SkinnableTextureAnimation
+                    {
+                        DefaultFrameLength = 1000f / 60f, // hardcoded 60fps for now
+                        Loop = true,
+                        Size = size,
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        FillMode = FillMode.Fill
+                    };
+
+                    foreach (var t in textures)
+                        animation.AddFrame(t);
+
+                    return animation;
+            }
+        }
+
+        public Texture[] GetTextures(TextureStore textureStore)
+        {
+            if (textureStore == null)
+                return Array.Empty<Texture>();
+
+            var textures = getTextures().ToArray();
+
+            if (textures.Length > 0)
+                return textures;
+
+            // if an animation was not allowed or not found, fall back to a sprite retrieval.
+            var singleTexture = textureStore.Get($@"Flags/{flag}");
+
+            return singleTexture != null
+                ? new[] { singleTexture }
+                : Array.Empty<Texture>();
+
+#nullable enable
+            IEnumerable<Texture> getTextures()
+            {
+                for (int i = 1; true; i++)
+                {
+                    Texture? texture;
+
+                    if ((texture = textureStore.Get(getFrameName(i))) == null)
+                        break;
+
+                    yield return texture;
                 }
-            }, true);
+            }
+#nullable disable
+
+            string getFrameName(int frameIndex)
+            {
+                string frameName = $@"Flags/{flag}_{frameIndex}";
+                Logger.Log($@"Getting frame {frameIndex} for texture {flag}: " + frameName, LoggingTarget.Runtime, LogLevel.Debug);
+                return frameName;
+            }
         }
     }
 }
