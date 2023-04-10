cd1747a1ce487014b4fef140dfb979c79cc681e1
ILW8 <daohengliu@gmail.com>
2023-04-10 23:28:45 +0100

    fix showcase TournamentLogo layering

--- osu.Game.Tournament/Screens/Showcase/ShowcaseScreen.cs
+++ osu.Game.Tournament/Screens/Showcase/ShowcaseScreen.cs
@@ -21,7 +21,6 @@ namespace osu.Game.Tournament.Screens.Showcase
         {
             AddRangeInternal(new Drawable[]
             {
-                new TournamentLogo(),
                 new TourneyVideo("showcase")
                 {
                     Loop = true,
@@ -40,7 +39,8 @@ namespace osu.Game.Tournament.Screens.Showcase
                         RelativeSizeAxes = Axes.Both,
                         Colour = new Color4(0, 255, 0, 255),
                     }
-                }
+                },
+                new TournamentLogo()
             });
         }
 
