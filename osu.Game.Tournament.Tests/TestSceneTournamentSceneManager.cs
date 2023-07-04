23edb66b0e2fbfdbf06068223d691fc43b3ad0a0
Dao Heng Liu <daohengliu@gmail.com>
2023-07-04 16:50:14 +0100

    (wip) add test to test switching screens in visualtests

--- osu.Game.Tournament.Tests/TestSceneTournamentSceneManager.cs
+++ osu.Game.Tournament.Tests/TestSceneTournamentSceneManager.cs
@@ -3,16 +3,31 @@
 
 #nullable disable
 
+using NUnit.Framework;
 using osu.Framework.Allocation;
+using osu.Game.Tournament.Screens.Gameplay;
+using osu.Game.Tournament.Screens.MapPool;
 
 namespace osu.Game.Tournament.Tests
 {
     public partial class TestSceneTournamentSceneManager : TournamentTestScene
     {
+        private readonly TournamentSceneManager tsm = new TournamentSceneManager();
+
         [BackgroundDependencyLoader]
         private void load()
         {
-            Add(new TournamentSceneManager());
+            Add(tsm);
+        }
+
+        [Test]
+        public void TestScreenSwitch()
+        {
+            AddStep("set screen to gameplay", () => tsm.SetScreen(typeof(GameplayScreen)));
+            AddWaitStep("wait for some time", 5);
+            AddStep("set screen to mappool", () => tsm.SetScreen(typeof(MapPoolScreen)));
+            AddWaitStep("wait for some time", 5);
+            AddStep("set screen to gameplay", () => tsm.SetScreen(typeof(GameplayScreen)));
         }
     }
 }
