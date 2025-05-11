689b0b10c630651fa722e222b6755fa5ad242a0b
Dao Heng Liu <daohengliu@gmail.com>
2025-05-11 02:06:14 +0100

    add toggle for 1v1 mode in seeding reveal screen

--- osu.Game.Tournament/Models/LadderInfo.cs
+++ osu.Game.Tournament/Models/LadderInfo.cs
@@ -41,6 +41,8 @@ namespace osu.Game.Tournament.Models
 
         public Bindable<bool> AutoProgressScreens = new BindableBool(true);
 
+        public Bindable<bool> Use1V1Mode = new Bindable<bool>(false);
+
         public Bindable<bool> SplitMapPoolByMods = new BindableBool(true);
 
         public Bindable<bool> DisplayTeamSeeds = new BindableBool();
