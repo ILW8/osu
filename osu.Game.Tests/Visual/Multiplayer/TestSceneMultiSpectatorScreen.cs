8a8855f69c31de4af2711d2d1cd0e4cddfb12f4c
Dao Heng Liu <daohengliu@gmail.com>
2025-07-25 05:56:57 +0100

    comment out obsoleted leaderboard test steps

--- osu.Game.Tests/Visual/Multiplayer/TestSceneMultiSpectatorScreen.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneMultiSpectatorScreen.cs
@@ -397,7 +397,7 @@ namespace osu.Game.Tests.Visual.Multiplayer
 
                 end(id);
                 AddUntilStep($"{id} area grayed", () => getInstance(id).Colour != Color4.White);
-                AddUntilStep($"{id} score quit set", () => getLeaderboardScore(id).HasQuit.Value);
+                // AddUntilStep($"{id} score quit set", () => getLeaderboardScore(id).HasQuit.Value);
                 sendFrames(getPlayerIds(count), 300);
             }
 
@@ -410,7 +410,7 @@ namespace osu.Game.Tests.Visual.Multiplayer
 
             AddAssert($"{PLAYER_1_ID} player instance still same", () => getInstance(PLAYER_1_ID).ChildrenOfType<Player>().Single() == player);
             AddAssert($"{PLAYER_1_ID} area still grayed", () => getInstance(PLAYER_1_ID).Colour != Color4.White);
-            AddAssert($"{PLAYER_1_ID} score quit still set", () => getLeaderboardScore(PLAYER_1_ID).HasQuit.Value);
+            // AddAssert($"{PLAYER_1_ID} score quit still set", () => getLeaderboardScore(PLAYER_1_ID).HasQuit.Value);
         }
 
         /// <summary>
@@ -561,7 +561,7 @@ namespace osu.Game.Tests.Visual.Multiplayer
 
         private PlayerArea getInstance(int userId) => spectatorScreen.ChildrenOfType<PlayerArea>().Single(p => p.UserId == userId);
 
-        private DrawableGameplayLeaderboardScore getLeaderboardScore(int userId) => spectatorScreen.Leaderboard.ChildrenOfType<DrawableGameplayLeaderboardScore>().Single(s => s.User?.OnlineID == userId);
+        // private DrawableGameplayLeaderboardScore getLeaderboardScore(int userId) => spectatorScreen.Leaderboard.ChildrenOfType<DrawableGameplayLeaderboardScore>().Single(s => s.User?.OnlineID == userId);
 
         private int[] getPlayerIds(int count) => Enumerable.Range(PLAYER_1_ID, count).ToArray();
     }
