ef19c6d3b30b9ab56bbd77830046befed1f68279
ILW8 <daohengliu@gmail.com>
2026-05-04 01:45:54 +0100

    add hold-to-trigger to disconnect button

--- osu.Game.Tournament/Components/MultiplayerRoomConnectionControls.cs
+++ osu.Game.Tournament/Components/MultiplayerRoomConnectionControls.cs
@@ -1,17 +1,18 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Threading.Tasks;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
 using osu.Framework.Input.Events;
 using osu.Framework.Logging;
 using osu.Game.Graphics;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Tournament.IPC;
 using osuTK;
-using osuTK.Graphics;
 
 namespace osu.Game.Tournament.Components
 {
@@ -38,6 +39,7 @@ namespace osu.Game.Tournament.Components
         private void load()
         {
             TourneyButton connectButton;
+            HoldToConfirmTourneyButton disconnectButton;
             TourneyButton reconnectButton;
             TournamentSpriteText statusText;
 
@@ -64,11 +66,27 @@ namespace osu.Game.Tournament.Components
                     PlaceholderText = "Password (optional)",
                     TabbableContentContainer = this,
                 },
-                connectButton = new TourneyButton
+                new Container
                 {
                     RelativeSizeAxes = Axes.X,
-                    Text = "Connect",
-                    Action = performConnectionToggle,
+                    Height = 40,
+                    Children = new Drawable[]
+                    {
+                        connectButton = new TourneyButton
+                        {
+                            RelativeSizeAxes = Axes.X,
+                            Text = "Connect",
+                            Action = performConnect,
+                        },
+                        disconnectButton = new HoldToConfirmTourneyButton
+                        {
+                            RelativeSizeAxes = Axes.X,
+                            Text = "Disconnect",
+                            Alpha = 0,
+                            Enabled = { Value = false },
+                            ConfirmAction = () => fireAndForget(multiplayerIpc.Disconnect()),
+                        },
+                    },
                 },
                 reconnectButton = new TourneyButton
                 {
@@ -86,13 +104,18 @@ namespace osu.Game.Tournament.Components
                 },
             };
 
-            roomIdTextBox.OnCommit += (_, _) => performConnectionToggle();
-            passwordTextBox.OnCommit += (_, _) => performConnectionToggle();
+            roomIdTextBox.OnCommit += (_, _) => performConnect();
+            passwordTextBox.OnCommit += (_, _) => performConnect();
 
             multiplayerIpc.IsConnected.BindValueChanged(connected =>
             {
-                connectButton.Text = connected.NewValue ? "Disconnect" : "Connect";
+                connectButton.Enabled.Value = !connected.NewValue;
+                disconnectButton.Enabled.Value = connected.NewValue;
                 reconnectButton.Enabled.Value = connected.NewValue;
+
+                connectButton.FadeTo(connected.NewValue ? 0 : 1, 200);
+                disconnectButton.FadeTo(connected.NewValue ? 1 : 0, 200);
+
                 statusText.Text = connected.NewValue
                     ? $"Connected (Room {multiplayerIpc.ConnectedRoomId.Value})"
                     : "Disconnected";
@@ -165,23 +188,19 @@ namespace osu.Game.Tournament.Components
             GetContainingFocusManager()?.ChangeFocus(roomIdTextBox);
         }
 
-        private void performConnectionToggle()
+        private void performConnect()
         {
             if (multiplayerIpc.IsConnected.Value)
-            {
-                fireAndForget(multiplayerIpc.Disconnect());
-            }
-            else
-            {
-                if (!long.TryParse(roomIdTextBox.Text, out long roomId))
-                {
-                    Logger.Log("[MultiplayerRoomConnectionControls] Invalid room ID", LoggingTarget.Runtime, LogLevel.Error);
-                    return;
-                }
+                return;
 
-                string? password = string.IsNullOrEmpty(passwordTextBox.Text) ? null : passwordTextBox.Text;
-                fireAndForget(multiplayerIpc.Connect(roomId, password));
+            if (!long.TryParse(roomIdTextBox.Text, out long roomId))
+            {
+                Logger.Log("[MultiplayerRoomConnectionControls] Invalid room ID", LoggingTarget.Runtime, LogLevel.Error);
+                return;
             }
+
+            string? password = string.IsNullOrEmpty(passwordTextBox.Text) ? null : passwordTextBox.Text;
+            fireAndForget(multiplayerIpc.Connect(roomId, password));
         }
 
         private static void fireAndForget(Task task)
@@ -190,6 +209,84 @@ namespace osu.Game.Tournament.Components
             {
                 Logger.Log(t.Exception?.GetBaseException().ToString() ?? "Room connection task failed.", LoggingTarget.Runtime, LogLevel.Error);
             }, TaskContinuationOptions.OnlyOnFaulted);
+        }
+
+        private partial class HoldToConfirmTourneyButton : TourneyButton
+        {
+            private const double hold_duration = 500;
+
+            public Action? ConfirmAction { get; init; }
+
+            private Box progressBox = null!;
+            private bool confirming;
+
+            [BackgroundDependencyLoader]
+            private void load(OsuColour colours)
+            {
+                BackgroundColour = colours.DangerousButtonColour;
+
+                Content.Add(progressBox = new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Width = 0,
+                    Blending = BlendingParameters.Additive,
+                    Depth = 0,
+                });
+            }
+
+            protected override bool OnClick(ClickEvent e) => true;
+
+            protected override bool OnMouseDown(MouseDownEvent e)
+            {
+                if (Enabled.Value)
+                    beginConfirm();
+
+                return base.OnMouseDown(e);
+            }
+
+            protected override void OnMouseUp(MouseUpEvent e)
+            {
+                abortConfirm();
+                base.OnMouseUp(e);
+            }
+
+            protected override void OnHoverLost(HoverLostEvent e)
+            {
+                abortConfirm();
+                base.OnHoverLost(e);
+            }
+
+            private void beginConfirm()
+            {
+                if (confirming)
+                    return;
+
+                confirming = true;
+
+                progressBox.ClearTransforms();
+                progressBox.ResizeWidthTo(1, hold_duration, Easing.Out).OnComplete(_ => confirm());
+            }
+
+            private void abortConfirm()
+            {
+                if (!confirming)
+                    return;
+
+                confirming = false;
+
+                progressBox.ClearTransforms();
+                progressBox.ResizeWidthTo(0, 200, Easing.InSine);
+            }
+
+            private void confirm()
+            {
+                if (!confirming)
+                    return;
+
+                confirming = false;
+                progressBox.ResizeWidthTo(0);
+                ConfirmAction?.Invoke();
+            }
         }
     }
 }
