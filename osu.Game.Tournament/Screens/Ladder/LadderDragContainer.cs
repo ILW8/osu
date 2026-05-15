33c05f3a3ac1ff7c6e32061bddd46476eb632b45
ILW8 <daohengliu@gmail.com>
2026-05-15 22:09:49 +0100

    adjust LadderDragContainer zoom range to 0.4-1.6

--- osu.Game.Tournament/Screens/Ladder/LadderDragContainer.cs
+++ osu.Game.Tournament/Screens/Ladder/LadderDragContainer.cs
@@ -29,8 +29,8 @@ namespace osu.Game.Tournament.Screens.Ladder
             this.MoveTo(target += e.Delta, 1000, Easing.OutQuint);
         }
 
-        private const float min_scale = 0.6f;
-        private const float max_scale = 1.4f;
+        private const float min_scale = 0.4f;
+        private const float max_scale = 1.6f;
 
         protected override bool OnScroll(ScrollEvent e)
         {
