3c9d60d09b7074b1e3529d80fc36a291eff455da
ILW8 <daohengliu@gmail.com>
2026-05-15 17:05:19 +0100

    Revert "widen mod icon margin to clear protect-icon wedge"
    This reverts commit b76175013a09b25064f0e617b29974ea4e116164.

--- osu.Game.Tournament/Components/TournamentBeatmapPanel.cs
+++ osu.Game.Tournament/Components/TournamentBeatmapPanel.cs
@@ -140,11 +140,7 @@ namespace osu.Game.Tournament.Components
                 {
                     Anchor = Anchor.CentreRight,
                     Origin = Anchor.CentreRight,
-                    // Right margin clears the protect-icon wedge (anchored top-right, ~35px
-                    // extent along the right edge after the 45° rotation). With margin=20 the
-                    // mod icon's top-right portion would overlap the wedge whenever both are
-                    // active on the same map.
-                    Margin = new MarginPadding { Right = 50 },
+                    Margin = new MarginPadding { Right = 20 },
                 });
             }
         }
