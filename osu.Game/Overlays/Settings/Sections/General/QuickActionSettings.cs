219b3953dafaf9ffe7c304f8fa1212cd805d3856
ILW8 <daohengliu@gmail.com>
2026-06-25 02:18:00 +0100

    Update vulnerable dependencies and port to SharpCompress 0.49.1 API
    Resolves NU1903/NU1902 build failures:
    - MessagePack 3.1.3 -> 3.1.7
    - SignalR packages 9.0.2 -> 9.0.17 (CVE-2026-45591)
    - SharpCompress 0.39.0 -> 0.49.1
    - AutoMapper held at 13.0.1 with NU1903 suppressed (14.0 licensing change;
      vulnerability does not affect us), matching upstream
    
    The SharpCompress bump required porting call sites to the new API
    (OpenArchive/CreateArchive statics, LzmaStream/BZip2Stream.Create factories,
    ArchiveEncoding object initializer, IWritableArchive/IArchiveEntry types).

--- osu.Game/Overlays/Settings/Sections/General/QuickActionSettings.cs
+++ osu.Game/Overlays/Settings/Sections/General/QuickActionSettings.cs
@@ -17,6 +17,8 @@ using osu.Game.Online.Chat;
 using osu.Game.Overlays.Notifications;
 using osu.Game.Utils;
 using SharpCompress.Archives.Zip;
+using SharpCompress.Common;
+using SharpCompress.Writers.Zip;
 
 namespace osu.Game.Overlays.Settings.Sections.General
 {
@@ -99,12 +101,12 @@ namespace osu.Game.Overlays.Settings.Sections.General
                 var logStorage = Logger.Storage;
 
                 using (var outStream = exportStorage.CreateFileSafely(archive_filename))
-                using (var zip = ZipArchive.Create())
+                using (var zip = ZipArchive.CreateArchive())
                 {
                     foreach (string? f in logStorage.GetFiles(string.Empty, "*.log"))
                         FileUtils.AttemptOperation(z => z.AddEntry(f, logStorage.GetStream(f), true), zip);
 
-                    zip.SaveTo(outStream);
+                    zip.SaveTo(outStream, new ZipWriterOptions(CompressionType.Deflate));
                 }
             }
             catch
